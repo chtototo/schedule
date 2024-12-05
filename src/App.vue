763813e6554a60f2a57ff6e2b725ed7dc35053b1
Amina <progristillidan@gmail.com>
2024-12-05 21:56:40 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -8,202 +8,163 @@ let showDayOptions = ref(false);
 let showOptions = ref(false);
 let selectedWeek = ref("Все недели");
 let selectedDay = ref("Вся неделя");
-let weekDays = computed(() =>
-  selectedWeek.value === "Все недели"
-    ? data.map((w) =>
-        w.week === selectedWeek.value ? w.days.map((d) => d.name) : ""
-      )
-    : Object.values(data)
-        .filter((w) => w.week === selectedWeek.value)[0]
-        .days.map((d) => d.name)
-);
-let color = computed(() =>
-  selectedWeek.value !== "Все недели" ? "#71717a" : "#d4d4d8"
-);
+let scheduleData = Object.values(data);
+let weekDays = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
 
 let schedule1 = ref([]);
 let schedule2 = ref([]);
 function filt() {
-  // schedule.value.push(
-  //   selectedWeek.value === "Все недели" ? data : data[selectedWeek.value]
-  // );
-  // console.log(data, schedule.value)
-  // if (selectedDay.value !== "Вся неделя") {
-  //   if (selectedWeek.value === "Все недели") {
-  //     let result = [];
-  //     result.push(
-  //       Array(schedule.value[0]["1"]).map((d) => d[selectedDay.value])
-  //     );
-  //     result.push(
-  //       Array(schedule.value[0]["2"]).map((d) => d[selectedDay.value])
-  //     );
-  //     schedule.value = result;
-  //   } else {
-  //     schedule.value = schedule.value.map((d) => d[selectedDay.value]);
-  //   }
-  // } else {
-  //   schedule.value = schedule.value[0];
-  // }
-  schedule1.value = [];
-  schedule2.value = [];
-  if (selectedWeek.value === "Все недели") {
-    schedule1.value = Object.values(data)[0].days;
-    schedule2.value = Object.values(data)[1].days;
-  } else if (selectedDay.value === "Вся неделя") {
-    if (selectedWeek.value === 1) {
-      schedule1.value = Object.values(data).filter(
-        (w) => w.week === selectedWeek.value
-      )[0].days;
-    } else {
-      schedule2.value = Object.values(data).filter(
-        (w) => w.week === selectedWeek.value
-      )[0].days;
-    }
+  schedule1.value = "";
+  schedule2.value = "";
+  let schedule_1 = getFullWeek(1).getDay(selectedDay.value);
+  let schedule_2 = getFullWeek(2).getDay(selectedDay.value);
+  if (selectedWeek.value === 1) {
+    schedule1.value = schedule_1;
+  } else if (selectedWeek.value === 2) {
+    schedule2.value = schedule_2;
   } else {
-    if (selectedWeek.value === 1) {
-      schedule1.value = Object.values(data)
-        .filter((w) => w.week === selectedWeek.value)[0]
-        .days.filter((d) => d.name === selectedDay.value);
-    } else {
-      schedule2.value = Object.values(data)
-        .filter((w) => w.week === selectedWeek.value)[0]
-        .days.filter((d) => d.name === selectedDay.value);
-    }
+    schedule1.value = schedule_1;
+    schedule2.value = schedule_2;
   }
-  console.log(schedule1.value);
 }
+
+function getFullWeek(week) {
+  return scheduleData.filter((w) => w.week === week)[0].days;
+}
+
+Array.prototype.getDay = function (day) {
+  if (day === "Вся неделя") {
+    return this;
+  }
+  return this.filter((d) => d.name === day);
+};
+
+function borderColor(type) {
+  let blue = "#60a5fa";
+  let indigo = "#818cf8";
+  let emerald = "#64748b";
+  switch (type) {
+    case "лек":
+      return blue;
+    case "лаб":
+      return emerald;
+    case "пр":
+      return indigo;
+    default:
+      return indigo;
+  }
+}
+
+console.log(0.1 + 0.2);
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-row relative">
-    <!-- sidebar -->
-    <div class="h-full fixed left-0 top-0 w-[50px] bg-zinc-900"></div>
-    <!-- inputs -->
-    <div
-      class="min-h-[100vh] w-fit absolute top-0 left-[50px] py-[100px] px-[50px] flex flex-col justify-center relative"
-    >
-      <!-- inputs container -->
-      <div class="grid grid-rows-3 gap-[20px] fixed">
-        <div class="flex flex-col relative">
-          <label>Неделя</label>
-          <div
-            @click="showWeekOptions = !showWeekOptions"
-            class="bg-white rounded w-[300px] p-[8px] h-[50px] cursor-pointer flex items-center"
-          >
-            <div class="h-min">{{ selectedWeek }}</div>
-          </div>
-          <div
-            v-if="showWeekOptions"
-            class="options"
-            @input="profile"
-            @click="showWeekOptions = !showWeekOptions"
-          >
-            <div @click="selectedWeek = 'Все недели'" class="option">
-              Все недели
-            </div>
-            <div @click="selectedWeek = 1" class="option">1</div>
-            <div @click="selectedWeek = 2" class="option">2</div>
-          </div>
+  <!-- sidebar -->
+  <div class="sidebar"></div>
+  <main>
+    <!-- inputs container -->
+    <div class="inputs-container">
+      <div class="input-container">
+        <label>Неделя</label>
+        <div @click="showWeekOptions = !showWeekOptions" class="input">
+          <div class="h-min">{{ selectedWeek }}</div>
         </div>
-
-        <div class="flex flex-col relative">
-          <label>День недели</label>
-          <div
-            @click="
-              selectedWeek === 'Все недели'
-                ? null
-                : (showDayOptions = !showDayOptions)
-            "
-            class="bg-white rounded w-[300px] p-[8px] h-[50px] cursor-pointer flex items-center"
-            :style="{ color: color }"
-            value="selectedDay"
-          >
-            <div class="h-min">{{ selectedDay }}</div>
-          </div>
-          <div
-            v-if="showDayOptions"
-            class="options"
-            @click="showDayOptions = !showDayOptions"
-            @input="profile"
-          >
-            <div class="option" @click="selectedDay = 'Вся неделя'">
-              Вся неделя
-            </div>
-            <div
-              v-for="day of weekDays"
-              :key="day"
-              @click="selectedDay = day"
-              class="option"
-            >
-              {{ day }}
-            </div>
-          </div>
-        </div>
-
-        <!-- <div class="flex flex-col">
-          <label>Курс</label>
-          <select class="select" @input="course">
-            <option disabled></option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-        </div>
-
-        <div class="flex flex-col">
-          <label>Неделя</label>
-          <select class="select" @input="week">
-            <option disabled></option>
-            <option>Первая</option>
-            <option>Вторая</option>
-          </select>
-        </div> -->
-        <button
-          class="uppercase rounded-full bg-indigo-900 w-fit px-[50px] py-[10px] text-zinc-300 shadow-md hover:opacity-[.8] absolute bottom-0 transition-[.3s] outline-none"
-          @click="filt()"
+        <div
+          v-if="showWeekOptions"
+          class="options"
+          @click="showWeekOptions = !showWeekOptions"
         >
-          get
-        </button>
+          <div @click="selectedWeek = 'Все недели'" class="option">
+            Все недели
+          </div>
+          <div @click="selectedWeek = 1" class="option">1</div>
+          <div @click="selectedWeek = 2" class="option">2</div>
+        </div>
       </div>
+
+      <div class="input-container">
+        <label>День недели</label>
+        <div
+          @click="showDayOptions = !showDayOptions"
+          class="input"
+          :style="{ color: color }"
+          value="selectedDay"
+          @input="filt()"
+        >
+          <div class="h-min">{{ selectedDay }}</div>
+        </div>
+        <div
+          v-if="showDayOptions"
+          class="options"
+          @click="showDayOptions = !showDayOptions"
+        >
+          <div class="option" @click="selectedDay = 'Вся неделя'">
+            Вся неделя
+          </div>
+          <div
+            v-for="day of weekDays"
+            :key="day"
+            @click="selectedDay = day"
+            class="option"
+          >
+            {{ day }}
+          </div>
+        </div>
+      </div>
+      <button class="btn" @click="filt()">get</button>
     </div>
 
     <!-- schedule -->
-    <div class="grid grid-cols-2 relative w-[100%] ml-[350px] mr-[20px]">
-      <div class="weekName">Первая</div>
-      <div class="week left-0">
-        <div class="days" v-for="day of schedule1" :key="day">
-          <div class="day">
-            <div class="day-name">{{ day.name }}</div>
-            <div class="grid grid-rows-6 gap-[10px]">
-              <div
-                v-for="(subject, index) of day.subjects"
-                :key="subject"
-                class="subjects"
-              >
-                <div class="subject-index">{{ index }}.</div>
-                <div class="subject" v-if="subject.name">
-                  <div class="">
-                    <div class="flex flex-row w-full mb-[5px]">
-                      <div class="subject-name">{{ subject.name }}</div>
-                      <div class="ml-[5px] text-zinc-500">
-                        {{ subject.type }}
+    <div class="schedule-container">
+      <div class="week">
+        <div class="weekName">Первая</div>
+        <div class="weekdays">
+          <div class="days" v-for="day of schedule1" :key="day">
+            <div class="day">
+              <div class="day-name">{{ day.name }}</div>
+              <div class="grid grid-rows-6 gap-[10px]">
+                <div
+                  v-for="(subject, index) of day.subjects"
+                  :key="subject"
+                  class="subjects"
+                >
+                  <div class="subject-index">{{ index }}.</div>
+                  <div
+                    class="subject"
+                    v-if="subject.name"
+                    :style="{ 'border-color': borderColor(subject.type) }"
+                  >
+                    <div class="">
+                      <div class="flex flex-row w-full mb-[5px] justify-between">
+                        <div class="flex flex-row">
+                          <div class="subject-name">{{ subject.name }}</div>
+                          <div class="ml-[5px] text-zinc-500">
+                            {{ subject.type }}
+                          </div>
+                        </div>
+                        <div
+                          v-if="subject.subgroup"
+                          class="ml-[10px] text-zinc-500"
+                        >
+                          {{ subject.subgroup }}п/г
+                        </div>
                       </div>
-                      <div
-                        v-if="subject.subgroup"
-                        class="ml-[10px] text-zinc-500"
-                      >
-                        {{ subject.subgroup }}п/г
-                      </div>
-                    </div>
-                    <div class="subject-info">
-                      <div class="">
-                        <span class="text-zinc-500">кабинет: </span
-                        >{{ subject.audience }}
-                      </div>
-                      <div class="">
-                        <span class="text-zinc-500">препод: </span
-                        >{{ subject.teacher }}
+                      <div class="subject-info">
+                        <div class="">
+                          <span class="text-zinc-500">кабинет: </span
+                          >{{ subject.audience }}
+                        </div>
+                        <div class="">
+                          <span class="text-zinc-500">препод: </span
+                          >{{ subject.teacher }}
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -213,41 +174,48 @@ function filt() {
           </div>
         </div>
       </div>
-
-      <div class="weekName">Вторая</div>
-      <div class="week right-0">
-        <div class="days" v-for="day of schedule2" :key="day">
-          <div class="day">
-            <div class="day-name">{{ day.name }}</div>
-            <div class="grid grid-rows-6 gap-[10px]">
-              <div
-                v-for="(subject, index) of day.subjects"
-                :key="subject"
-                class="subjects"
-              >
-                <div class="subject-index">{{ index }}.</div>
-                <div class="subject" v-if="subject.name">
-                  <div class="">
-                    <div class="flex flex-row w-full mb-[5px]">
-                      <div class="subject-name">{{ subject.name }}</div>
-                      <div class="ml-[5px] text-zinc-500">
-                        {{ subject.type }}
+      <div class="week">
+        <div class="weekName">Вторая</div>
+        <div class="weekdays">
+          <div class="days" v-for="day of schedule2" :key="day">
+            <div class="day">
+              <div class="day-name">{{ day.name }}</div>
+              <div class="grid grid-rows-6 gap-[10px]">
+                <div
+                  v-for="(subject, index) of day.subjects"
+                  :key="subject"
+                  class="subjects"
+                >
+                  <div class="subject-index">{{ index }}.</div>
+                  <div
+                    class="subject"
+                    v-if="subject.name"
+                    :style="{ 'border-color': borderColor(subject.type) }"
+                  >
+                    <div class="">
+                      <div class="flex flex-row w-full mb-[5px] justify-between">
+                        <div class="flex flex-row">
+                          <div class="subject-name">{{ subject.name }}</div>
+                          <div class="ml-[5px] text-zinc-500">
+                            {{ subject.type }}
+                          </div>
+                        </div>
+                        <div
+                          v-if="subject.subgroup"
+                          class="ml-[10px] text-zinc-500"
+                        >
+                          {{ subject.subgroup }}п/г
+                        </div>
                       </div>
-                      <div
-                        v-if="subject.subgroup"
-                        class="ml-[10px] text-zinc-500"
-                      >
-                        {{ subject.subgroup }}п/г
-                      </div>
-                    </div>
-                    <div class="subject-info">
-                      <div class="">
-                        <span class="text-zinc-500">кабинет: </span
-                        >{{ subject.audience }}
-                      </div>
-                      <div class="">
-                        <span class="text-zinc-500">препод: </span
-                        >{{ subject.teacher }}
+                      <div class="subject-info">
+                        <div class="">
+                          <span class="text-zinc-500">кабинет: </span
+                          >{{ subject.audience }}
+                        </div>
+                        <div class="">
+                          <span class="text-zinc-500">препод: </span
+                          >{{ subject.teacher }}
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -258,5 +226,5 @@ function filt() {
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
